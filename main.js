4cfd002c7d5492707c78db916b66b21b840b576f
Jay Viannay <jay@air-de-jay.home>
2024-02-06 13:32:58 +0100

    init project

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('mains.js loaded');
+
+function checkCardNumber(event) {
+    event.preventDefault();
+    let cardNumber = document.getElementById('cardNumber').value;
+    // your code here
+}
